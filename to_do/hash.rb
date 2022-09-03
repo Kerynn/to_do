0230d2369be206866138410ca3b1b7e227a5f9ae
Kerynn Davis <kerynn.davis@gmail.com>
2022-09-03 08:24:39 -0700

    Add hash

---
+++ to_do/hash.rb
@@ -0,0 +1,20 @@
+# inventory = {
+#   "dairy" => ["milk", "cheese", "ice cream"],
+#   "meat" => ["steak", "sausage", "chicken"],
+#   "vegetables" => ["corn", "kale", "carrots"]
+# }
+#
+#
+# tweet = {
+#   "twitter_user_name" => ["MechEngSanchez"],
+#   "Likes" => 21,
+#   "tweet_date" => ["01/25/2022"],
+#   "tweet_time" => ["4:06 AM"]
+# }
+
+
+arrays = ["Nicole", "Jim", "Ilana", "Joe", "Kim"]
+
+p arrays[2]
+p arrays[4]
+p arrays[0]
